10ea4cb7952eb0498542fc94af433f4d55f8465d
Vincent Nduri <ndurivo@gmail.com>
2023-02-09 00:44:25 +0300

    Creates all necessary section components and css

---
+++ src/sections/floating-nav/FloatingNav.jsx
@@ -0,0 +1,9 @@
+import './floating-nav.css';
+
+const FloatingNav = () => {
+  return (
+    <section id="floating-nav"></section>
+  )
+}
+
+export default FloatingNav;
---
+++ src/sections/header/Header.jsx
@@ -0,0 +1,10 @@
+import './header.css';
+
+
+const Header = () => {
+  return (
+    <header id="header"><h2>Header</h2></header>
+  )
+}
+
+export default Header;
