a2a536f62621dddc915d97fde50ab170420d52e7
Vincent Nduri <ndurivo@gmail.com>
2023-02-10 14:31:16 +0300

    Adds icons, and navbar section data and styling

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,10 +1,27 @@
-import './navbar.css';
+import "./navbar.css";
+import data from "./data.js";
+import { IoIosColorPalette } from 'react-icons/io';
+
 
 
 const Navbar = () => {
   return (
-    <nav><h2>Navbar</h2></nav>
-  )
-}
+    <nav>
+      <div className="container nav__container">
+        <a href="index.html" className="nav__logo">
+          <img src="" alt="logo" />
+        </a>
+        <ul className="nav__menu">
+          {data.map((item) => (
+            <li key={item.id}>
+              <a href={item.link}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+        <button id="theme__icon"><IoIosColorPalette /> </button>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
