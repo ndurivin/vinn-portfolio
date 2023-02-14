8e1953e2bd0f00d7b7105fa4e0d83d2aaaabd711
Vincent Nduri <ndurivo@gmail.com>
2023-02-14 12:44:57 +0300

    Adds the services data

---
+++ src/sections/services/data.js
@@ -0,0 +1,21 @@
+import { SiAltiumdesigner } from 'react-icons/si';
+import { TbBrandAdobe } from 'react-icons/tb';
+import { GiServerRack } from 'react-icons/gi';
+import { HiOutlineComputerDesktop } from 'react-icons/hi';
+
+
+const data = [
+    {
+        id: 1, icon: <TbBrandAdobe /> , title: 'UI/UX Design', desc:'I offer modern and intuitive designs. I apply the best industry-standards rules in making sure users have using the products.'
+    },
+    {
+        id: 2, icon: <SiAltiumdesigner /> , title: 'Front-end Development', desc: 'I will ensure your final product looks good and is accessible in all devices, including mobiles, tablets, and desktops.'
+    },
+    {
+        id: 3, icon: <GiServerRack /> , title: 'Back-end Development', desc: 'The logis and seciruty of your business and product must be the priority. We take security seriously from the start of the project to its end. I ensure security and efficiency.'
+    },
+    {
+        id: 4, icon: <HiOutlineComputerDesktop /> , title: 'IT Support', desc: "A good product on a faulty system isn't the right approach to follow. I will ensure that your systems are equally in their optimum states of operation, both in software and hardware aspecs."
+    },
+
+]
