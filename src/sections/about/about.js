575a4d5df56f847ff316fc036775ada2ff7d6673
Vincent Nduri <ndurivo@gmail.com>
2023-02-14 09:51:47 +0300

    Fixed responsiveness in about section

--- src/sections/about/about.js
+++ src/sections/about/about.js
@@ -1,11 +1,12 @@
 import { TbAward } from 'react-icons/tb';
-import { GrProjects } from 'react-icons/gr';
+import { FaProjectDiagram } from 'react-icons/fa';
 import { HiOutlineUsers } from 'react-icons/hi';
 
 const data = [
     {id: 1, icon: <TbAward />, title: 'Experience', desc: '3+ Years'},
-    {id: 2, icon: <GrProjects />, title: 'Projects', desc: '20+ Completed'},
+    {id: 2, icon: <FaProjectDiagram />, title: 'Projects', desc: '20+ Completed'},
     {id: 3, icon: <HiOutlineUsers />, title: 'Clients', desc: '10+ Happy Clients'}
 ]
 
 export default data;
+
