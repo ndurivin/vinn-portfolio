acb8286256289a7767269e788ff85a09a2620cd2
Vincent Nduri <ndurivo@gmail.com>
2023-02-20 17:33:02 +0300

    Add styling to the FAQ section

--- src/sections/faqs/FAQS.jsx
+++ src/sections/faqs/FAQS.jsx
@@ -2,18 +2,20 @@ import React, { useState } from "react";
 import "./faqs.css";
 import SingleQuery from "./SingleQuery";
 import data from "./data";
+import Card from "../../components/Card";
 
 const FAQS = (props) => {
-  // const [data, setData] = useState(data);
   return (
     <section id="faqs">
       <h2>Frequently Asked Questions</h2>
       <p>Get all the answers to your questions answered by the click.</p>
-      <div className="container faq__container">
-        {data.map((item) => {
-          return <SingleQuery key={item.id} {...item} />;
-        })}
-      </div>
+      <Card>
+        <div className="container faq__container">
+          {data.map((item) => {
+            return <SingleQuery key={item.id} {...item} />;
+          })}
+        </div>
+      </Card>
     </section>
   );
 };
--- src/sections/faqs/SingleQuery.jsx
+++ src/sections/faqs/SingleQuery.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 import Card from "../../components/Card";
-import "./singlequery.css";
 
 const SingleQuery = ({ question, answer }) => {
   const [showData, setShowData] = useState(false);
 
   return (
     <div className="container sq__container">
-      <Card className="sq light">
+      <Card className="sq white">
         <div className="sq__details">
           <h4>{question}</h4>
-          <button onClick={() => setShowData(!showData)}>
-            {showData ? <AiOutlineMinus /> : <AiOutlinePlus />}
-          </button>
+          <span>
+            <button onClick={() => setShowData(!showData)}>
+              {showData ? <AiOutlineMinus /> : <AiOutlinePlus />}
+            </button>
+          </span>
         </div>
         <p>{showData && <p>{answer}</p>}</p>
       </Card>
